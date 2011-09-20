20f0d18c16a29dcdd515ea6716ff1582582916c5
Duncan Gough <duncan@suttree.com>
2011-09-20 15:50:11 +0100

    Commenting out the redis server check

--- redis/recipes/default.rb
+++ redis/recipes/default.rb
@@ -1,8 +1,8 @@
 include_recipe "redis::client"
 
-execute "ensure-redis-is-running" do
-  command %Q{
-    /etc/init.d/redis-server start /etc/redis.conf
-  }
-  not_if "pgrep redis-server"
-end
+#execute "ensure-redis-is-running" do
+#  command %Q{
+#    /etc/init.d/redis-server start /etc/redis.conf
+#  }
+#  not_if "pgrep redis-server"
+#end
