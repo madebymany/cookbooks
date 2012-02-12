73f9479417b858412f18219e73842e5847707c7f
Stuart Eccles <stuart@madebymany.co.uk>
2012-02-12 21:01:27 +0000

    redis downloaded

--- redis/attributes/redis.rb
+++ redis/attributes/redis.rb
@@ -1,7 +1,9 @@
 default.redis[:user] = "redis"
 default.redis[:group] = "redis"
 
-default.redis[:version] = "2:1.2.0-1"
+default[:redis][:home_dir] = "/usr/local/share/redis"
+default[:redis][:install_url] = 'http://redis.googlecode.com/files/redis-2.4.7.tar.gz'
+
 default.redis[:port] = 6379
 default.redis[:bind_address] = "0.0.0.0"
 default.redis[:timeout] = 300
