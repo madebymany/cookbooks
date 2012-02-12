d002430ad902c2825593484cbd8220212b73f1fa
Stuart Eccles <stuart@madebymany.co.uk>
2012-02-12 19:10:51 +0000

    trying resource:

--- rails_app/libraries/mashing.rb
+++ rails_app/libraries/mashing.rb
@@ -1,10 +1,12 @@
 
 class Chef
-    class Recipe
-      def fix_mash_to_hash(mash)
-        hash = {}
-        mash.each { |key, value| hash[key] = value.is_a?(Mash) ? fix_mash_to_hash(value) : value }
-        hash
+    class Resource
+      class Template
+        def fix_mash_to_hash(mash)
+          hash = {}
+          mash.each { |key, value| hash[key] = value.is_a?(Mash) ? fix_mash_to_hash(value) : value }
+          hash
+        end
       end
     end
 end
