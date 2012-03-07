0e2631f29d571cfc9d00e0594f656838032a3e51
Alex Tomlins <alex.tomlins@unboxedconsulting.com>
2012-03-07 11:10:02 +0000

    Doh! it's owner, not user when creating files etc.

--- scout_agent/recipes/activemq_plugin.rb
+++ scout_agent/recipes/activemq_plugin.rb
@@ -7,14 +7,14 @@ if node[:recipes].include?('activemq')
   # In case it doesn't already exist
   directory "/home/#{node.scout_agent.user}/.scout" do
     mode "0755"
-    user node.scout_agent.user
+    owner node.scout_agent.user
     group node.scout_agent.group
   end
 
   cookbook_file "/home/#{node.scout_agent.user}/.scout/activemq_queue.rb" do
     source "plugins/activemq_queue.rb"
     mode "0755"
-    user node.scout_agent.user
+    owner node.scout_agent.user
     group node.scout_agent.group
   end
 
