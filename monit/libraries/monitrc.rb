1b301f7ca75dea1f096ba4f4ad4790cc03534a44
Stuart Eccles <stuart@madebymany.co.uk>
2012-03-29 11:04:38 +0100

    slight change to monit template override

--- monit/libraries/monitrc.rb
+++ monit/libraries/monitrc.rb
@@ -3,7 +3,8 @@ class Chef
     # name        The name of the service.  Looks for a template named NAME.conf
     # variables   Hash of variables to pass to the template
     # reload      Reload monit so it notices the new service.  :delayed (default) or :immediately.
-    def monitrc(name, variables={}, reload = :delayed, template_source = "#{name}.conf.erb}")
+    def monitrc(name, variables={}, reload = :delayed, template_source = nil) 
+      template_source = "#{name}.conf.erb}" if template_source.nil?
       log "Making monitrc for: #{name}"
       template "/etc/monit/conf.d/#{name}.conf" do
         owner "root"
