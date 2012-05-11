60b32fafb0dc7731dd9d3d57b80af0b399c3f145
Stuart Eccles <stuart@madebymany.co.uk>
2012-05-11 14:30:12 +0100

    adding a ulimit cookbook

---
+++ ulimit/recipes/default.rb
@@ -0,0 +1,20 @@
+script "include sudoers.d" do
+    interpreter "bash"
+    user "root"
+    cwd "/etc/pam.d"
+    code "echo 'session    required   pam_limits.so' >> /etc/pam.d/su"
+    not_if "grep -E '^session    required   pam_limits.so' /etc/pam.d/su"
+end
+
+if node[:ulimit]
+  node[:ulimit].each do |userconf|
+    template "/etc/security/limits.d/#{userconf[:user]}.conf" do
+      source "limits.erb"
+      mode 0440
+      owner "root"
+      group "root"
+      variables(:userconf => userconf)
+    end
+  end
+end
+
