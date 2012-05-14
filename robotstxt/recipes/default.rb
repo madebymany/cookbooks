a1b59f7eef974dde19967e6f4d457adf8f4a3923
Stuart Eccles <stuart@madebymany.co.uk>
2012-05-14 12:45:45 +0100

    create dir

--- robotstxt/recipes/default.rb
+++ robotstxt/recipes/default.rb
@@ -1,6 +1,13 @@
+directory node[:robotstxt][:location] do
+  owner node[:robotstxt][:user]
+  group node[:robotstxt][:group] 
+  mode "0755"
+  action :create
+end
+
 template "#{node[:robotstxt][:location]}/robots.txt" do
   source "robots.txt.erb"
-  mode 0440
+  mode "0644"
   owner node[:robotstxt][:user]
   group node[:robotstxt][:group]
 end
