58f55d755e95806d01ae7c486c523697b1653a19
Stuart Eccles <stuart@madebymany.co.uk>
2012-07-27 15:34:42 +0100

    make monit restart immediately when asked in chef notify

--- monit/libraries/monitrc.rb
+++ monit/libraries/monitrc.rb
@@ -2,7 +2,7 @@ class Chef
   class Recipe
     # name        The name of the service.  Looks for a template named NAME.conf
     # variables   Hash of variables to pass to the template
-    # reload      Reload monit so it notices the new service.  :delayed (default) or :immediately.
+    # reload      Reload monit so it notices the new service.  :delayed (default) or :immediately. (actually doesn't do anything, doing restarts immediately as only this seems to work)
     def monitrc(name, variables={}, reload = :delayed, template_source = nil) 
       template_source = "#{name}.conf.erb}" if template_source.nil?
       log "Making monitrc for: #{name}"
@@ -12,7 +12,7 @@ class Chef
         mode 0644
         source template_source
         variables variables
-        notifies :restart, resources(:service => "monit"), reload
+        notifies :restart, resources(:service => "monit"), :immediate
         action :create
       end
     end
