db6e0dc9fc7cb5e83470c85211c402269ced7128
Stuart Eccles <stuart@madebymany.co.uk>
2012-08-28 12:08:00 +0100

    a nephelae cookbook

---
+++ nephelae/attributes/nephelae.rb
@@ -0,0 +1,4 @@
+default[:nephelae][:conf_file]       = "/etc/nephelae.yml"
+default[:nephelae][:log_dir]         = "/var/log"
+default[:nephelae][:pid_dir]         = "/var/run"
+default[:nephelae][:log_level]       = "warn"
