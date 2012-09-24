53d8e2ade13545b1d5177cfb5a9e5d73d8a1b992
Dan Brown <dan@madebymany.co.uk>
2012-09-24 16:45:48 +0100

    nodejs can be installed from ppa

---
+++ nodejs/recipes/ppa.rb
@@ -0,0 +1,19 @@
+raise "Unsupported platform" unless node[:platform] == 'ubuntu'
+
+bash "add node.js repository" do
+  code <<-END
+  set -e
+  apt-get install -y python-software-properties
+  apt-add-repository -y ppa:chris-lea/node.js
+  apt-get update
+  END
+
+  not_if "grep -R node.js /etc/apt/sources.list /etc/apt/sources.list.d >/dev/null"
+end
+
+%w{nodejs npm}.each do |p|
+  package p do
+    action :upgrade
+  end
+end
+
