7cf8d97568d015bea6769b1a0b3cdf3f44754c85
Ryan McGrath <ryan@madebymany.co.uk>
2013-08-08 15:02:37 +0100

    combined install of the apt package into one bash script

--- apt-s3/recipes/default.rb
+++ apt-s3/recipes/default.rb
@@ -7,22 +7,12 @@ distribution = node["apts3"]["distribution"]
 components = node["apts3"]["components"]
 architectures = node["apts3"]["architectures"]
 
-bash "add mxm package repo" do 
+bash "installing apt-s3 package" do 
   code <<-EOH
   add-apt-repository -y ppa:mxmops/packages 
   apt-get update -qq
-  EOH
-  action :run
-end
-
-apt_package "installing apt-s3 method for apt" do
-  name "apt-transport-s3"
-  action :install
-end
-
-bash "adding s3 apt repo" do
-  code <<-EOH
-    echo "deb [arch=#{architectures}] s3://#{access_id}:[#{secret_key}]@s3.amazonaws.com/#{bucket} #{distribution} #{components}" | sudo tee /etc/apt/sources.list.d/s3.amazonaws.com.#{bucket}.list
+  apt-get install -y --force-yes apt-transport-s3
+  echo "deb [arch=#{architectures}] s3://#{access_id}:[#{secret_key}]@s3.amazonaws.com/#{bucket} #{distribution} #{components}" | sudo tee /etc/apt/sources.list.d/s3.amazonaws.com.#{bucket}.list
   EOH
   action :run
 end
