49cffc5d4234fe7acf57c42848052a2296010bde
Ryan McGrath <ryan@madebymany.co.uk>
2013-08-09 10:37:52 +0100

    moved ruby 2 into it's own recipe, removed ruby installation from passenger recipe

--- passenger_apache2/attributes/default.rb
+++ passenger_apache2/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:passenger][:version]     = "4.0.2"
+default[:passenger][:version]     = "4.0.10"
 default[:passenger][:max_pool_size] = "6"
 default[:passenger][:root_path]   = "#{languages[:ruby][:gems_dir]}/gems/passenger-#{passenger[:version]}"
 default[:passenger][:module_path] = "#{passenger[:root_path]}/libout/apache2/mod_passenger.so"
--- ruby2/recipes/default.rb
+++ ruby2/recipes/default.rb
@@ -1,20 +1,5 @@
 include_recipe "apts3"
 
-#apt_package "installing libssl debian" do
-  #name "libssl-dev"
-  #action :install
-#end
-
-#apt_package "installing openssl debian" do
-  #name "openssl"
-  #action :install
-#end
-
-#apt_package "installing libreadline debian" do
-  #name "libreadline-dev"
-  #action :install
-#end
-
 bash "installing ruby 2" do
   code <<-EOH
   apt-get install -y --force-yes ruby-2-mxm
